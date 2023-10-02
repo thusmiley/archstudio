161906586cf922a84349191deb9b22b260024206
Thu Dang <>
2023-10-02 18:02:40 -0500

    add carousel control buttons for mobile

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -15,7 +15,8 @@ const HomeHero = () => {
       {/* carousel */}
       <div className="relative md:mx-[13%]">
         <div id="carousel" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators xl:flex xl:ml-[-80px] xl:justify-start">
+          {/* indicators */}
+          <ol class="hidden md:flex carousel-indicators xl:ml-[-80px] xl:justify-start">
             <li data-target="#carousel" data-slide-to="0" class="active">
               01
             </li>
@@ -29,6 +30,7 @@ const HomeHero = () => {
               04
             </li>
           </ol>
+
           <div class="carousel-inner">
             {/* project paramour */}
             <div className="carousel-item active home-hero-bg-paramour px-[5%] py-[110px] min-w-full w-full h-[560px] md:h-[720px] md:py-[180px] md:px-[58px] xl:px-[190px]">
@@ -88,6 +90,15 @@ const HomeHero = () => {
               </a>
             </div>
           </div>
+
+          {/* next prev buttons */}
+          <a className="carousel-control-prev md:hidden" href="#carousel" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon md:hidden" aria-hidden="true"></span>
+          </a>
+          <a className="carousel-control-next md:hidden" href="#carousel" role="button" data-slide="next">
+            <span className="carousel-control-next-icon md:hidden" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
         </div>
       </div>
     </section>
